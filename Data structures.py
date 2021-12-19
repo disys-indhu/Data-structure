b7d820d7c507212c129183eb8e576ea774be67b7
disys-indhu <95426058+disys-indhu@users.noreply.github.com>
2021-12-19 05:34:43 -0800

    Add files via upload

---
+++ Data structures.py
@@ -0,0 +1,14 @@
+class coder:
+	loc = 100
+	def __init__(self):
+		self.error=20
+
+indhu=coder()#object creation
+
+print(indhu.error)
+
+activity=(40,50,500,20,590)
+print(activity[2])
+game=[1,2,3]
+print(game[1])
+
